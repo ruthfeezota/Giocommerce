20e833fd7b32ec7d54a30a9215b158ecfa45211d
ruthfeezota <110999344+ruthfeezota@users.noreply.github.com>
2023-02-17 20:10:01 -0600

    firebase deploy

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,20 +2,20 @@ import React from "react"
 import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 
-const Footer = () => <footer className="page-footer bg-black font-small text-white sticky-bottom">
-    <Row className="justify-content-md-center me-3 p-5">
+const Footer = () => <footer className="bg-black text-white fixed-bottom">
+    <Row className="justify-content-md-center me-2 py-3">
     <div className="container-fluid text-center text-md-left">
-        <div className="row col-md-12 mt-md-12 mt-2">
+        <div className="row col-md-12 mt-md-12">
             {/* <div className="col-md-12 mt-md-0 mt-3"> */}
 
                 <Col xs>
-                <h4>EMAIL LIST</h4>
+                <h6>EMAIL LIST</h6>
                 </Col>
                 <Col xs={{ order: 12 }}>
-                <h4>LAUNCH DAY</h4>
+                <h6>LAUNCH DAY</h6>
                 </Col>
                 <Col xs={{ order: 1 }}>
-                <h4>GAME PLAN</h4>
+                <h6>GAME PLAN</h6>
                 </Col>
             </div>
         </div>
--- src/Components/landingEmail.js
+++ src/Components/landingEmail.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import { Row } from "react-bootstrap";
+import '../index.css';
 
 
 
@@ -13,14 +14,15 @@ function LandEmail() {
     <Form className="bg-black text-white text-center align-center">
       <Row className="justify-content-md-center">
         <Form.Group className="mb-3" controlId="landingEmail">
-          <Form.Label >JOIN OUR EMAIL LIST</Form.Label>
-          <Col sm="2">
-          <Form.Control className="smaller-input" type="email" placeholder="EMAIL" />
-          </Col>
-        </Form.Group>
-        <Button variant="warning" type="submit">
+          <Form.Label className="fs-5 mt-2 text-warning">JOIN OUR EMAIL LIST</Form.Label>
+          <Col sm="2" className="offset-1 col-lg-3 offset-lg-5 div-wrapper d-flex justify-content-center align-items-center">
+          <Form.Control className="square rounded-0 border border-white bg-black" type="email" placeholder="EMAIL"/>
+          <Button variant="warning" type="submit" className="ml-2 square rounded-0">
           SUBMIT
         </Button>
+          </Col>
+        </Form.Group>
+
     </Row>
     </Form>
 
