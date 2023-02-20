93aa1982993c405cd2cbd7f3e8d9da5adc6c9ae9
ruthfeezota <110999344+ruthfeezota@users.noreply.github.com>
2023-02-19 22:23:27 -0600

    redo video section

--- src/Components/VideoSection.js
+++ src/Components/VideoSection.js
@@ -2,28 +2,32 @@ import React from "react";
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles.css";
-import YoutubeEmbed from "./YoutubeEmbed";
+import { Container } from 'react-bootstrap'
+
 
 const VideoSec = () => <header style={{ paddingLeft: 0 }}>
+  
 <div
   className='p-5 text-center bg-image'
-  style={{ backgroundImage: "url('https://i.ibb.co/s5g9YpN/home-Page-Cover.png')", height: 400 }}
->
-  <div className='mask'>
+  style={{ backgroundImage: "url('https://i.ibb.co/s5g9YpN/home-Page-Cover.png')", height: 600 }}>
+    <Container>
+  <div className="">
     <div className='align-items-center h-100'>
       <div className='text-white'>
         <h1 className='mb-3'>ABOUT GIOCOMMERCE</h1>
       </div>
-      <div className='text-white col-md-4'>
+      <div className='text-white col-md-4' style={{top: 80}}>
         <h5>Diverse small business owners have struggled to get the funding they need to begin, support, and grow their business. Many banks have successfully supported and funded diverse small businesses by providing Special Purpose Credit Programs (SPCP) to make loans that consider expanded credit criteria.
         </h5>
       </div>
-      <div className="App">
-      <YoutubeEmbed embedId="rokGy0huYEA" />
+      <div class="ratio ratio-4x3 ms-auto " style={{ width: 660, height: 300, top: -150}}>
+        <iframe src="https://www.youtube.com/embed/nf5A40WXLrc" title="Gioocommerce Demo Video" allowFullScreen></iframe>
+      </div>
+        
     </div>
     </div>
+    </Container>
   </div>
-</div>
-</header>
+  </header>
 
 export default VideoSec;
